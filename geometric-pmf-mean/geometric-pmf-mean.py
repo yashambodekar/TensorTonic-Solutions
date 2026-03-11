aa33dda89993887acbb566a87f9e5a09102e5696
yashambodekar <144942722+yashambodekar@users.noreply.github.com>
2026-03-12 01:59:40 +0530

    Update solution for geometric-pmf-mean

--- geometric-pmf-mean/geometric-pmf-mean.py
+++ geometric-pmf-mean/geometric-pmf-mean.py
@@ -6,10 +6,7 @@ def geometric_pmf_mean(k, p):
     """
     # Write code here
     k = np.array(k)
-    ans = []
-    for i in k:
-        sum = ((1 - p) ** (i - 1)) * p
-        ans.append(sum)
+    ans = ((1 - p) ** (k - 1)) * p
     mean = 1 / p
     res = (np.array(ans) , mean)
     return res
